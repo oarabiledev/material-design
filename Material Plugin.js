e058b68807f4586a72745275d00045e84319207d
Oarabile Koore <153186345+oarabilekoore@users.noreply.github.com>
2024-12-19 21:47:42 +0200

    Update Material Plugin.js
    Signed-off-by: Oarabile Koore <153186345+oarabilekoore@users.noreply.github.com>

--- Material Plugin.js
+++ Material Plugin.js
@@ -1,38 +1,31 @@
 //ide.MakePlugin('Material')
-app.Script("Material.js");
+app.Script('Material.js')
 
-function OnStart() {
-    mdui.ScaffoldAppTheme("dark", "outlined");
-
-    let lay = mdui.CreateLayout("linear", "FillXY, VCenter");
-    lay.SetChildMargins(0, 0.02);
-
-    let btn = mdui.AddButton(lay, "Hello World", 0.85, -1);
-
-    btn.SetOnTouch(() => {
-        pg.SetValue(70);
-    });
-
-    let fab = mdui.AddIconFAB(lay, "edit").SetOnTouch(() => {
-        //pgl.SetValue(100)
-    });
-
-    let icon = mdui.AddIconButton(lay, "edit", "filled");
-
-    let rad = mdui.AddRadioButton(lay, false).SetOnCheck(function () {
-        app.ShowPopup("rad: ");
-    });
-
-    let chk = mdui.AddCheckBox(lay, false).SetOnCheck(function () {
-        app.ShowPopup("chk: ");
-    });
-
-    let sw = mdui.AddSwitch(lay, false).SetOnCheck(() => {
-        mdui.SetTheme("light");
-    });
-
-    let pg = mdui.AddProgressBar(lay, 20, 0.8);
-
-    //let pgl = mdui.AddProgressBar(lay, null, 0.8, 'linearintermediate')
-    app.AddLayout(lay);
+function OnStart(){
+    
+    MUISetBackColor('white'), MUIScaffoldAppTheme('light', 'outlined');
+    
+    const homePage = MUICreateLayout('linear', 'fillxy, vcenter')
+    homePage.SetChildMargins(0, 0.05)
+    
+    
+    let btn = MUIButton(homePage, 'Hello World', 0.8, -1, 'filled')
+    btn.SetOnTouch(()=>{
+       MUISnackBar('Progress Updated',0.85).Show()
+    })
+    
+    MUIFAB(homePage, 'edit')
+    
+    MUIIconButton(homePage, 'bookmark', 'filled', 
+    'outlined')
+    
+    MUIRadioButton(homePage, false)
+    
+    MUICheckBox(homePage, true)
+    
+    MUIProgressBar(homePage, 28, 0.2)
+    
+    MUISwitch(homePage, true)
+    
+    app.AddLayout(homePage)
 }
